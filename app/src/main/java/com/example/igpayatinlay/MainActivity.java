ef028d18ee07e9ff153831c1df3cd5ce5661e615
Gawesomer <jj.jouband@gmail.com>
2019-11-21 14:23:07 -0600

    Added text to speech functionality.

--- app/src/main/java/com/example/igpayatinlay/MainActivity.java
+++ app/src/main/java/com/example/igpayatinlay/MainActivity.java
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.speech.tts.TextToSpeech;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -18,19 +21,23 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Activity MyActivity;
+    private Activity mActivity;
+    private EditText inputBox;
     private TextToSpeech textToSpeech;
+    private static final int TTS_REQUEST = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        MyActivity = this;
+        mActivity = this;
+        inputBox = findViewById(R.id.inputTextBox);
         ConstraintLayout masterContainer = findViewById(R.id.constraintLayout);
         setupUI(masterContainer);
 
@@ -50,7 +57,7 @@ public class MainActivity extends AppCompatActivity {
 
                     if (ttsLang == TextToSpeech.LANG_MISSING_DATA
                             || ttsLang == TextToSpeech.LANG_NOT_SUPPORTED) {
-                        Log.e("TTS", "The Language is not supported!");
+                        Log.e("TTS", "The language is not supported!");
                     } else {
                         Log.i("TTS", "Language Supported.");
                     }
@@ -62,8 +69,43 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    public void setupUI(View view) {
+
+        // Set up touch listener for non-text box views to hide keyboard.
+        if (!(view instanceof EditText)) {
+            view.setOnTouchListener(new View.OnTouchListener() {
+                public boolean onTouch(View v, MotionEvent event) {
+                    hideSoftKeyboard(mActivity);
+                    return false;
+                }
+            });
+        }
+
+        //If a layout container, iterate over children and seed recursion.
+        if (view instanceof ViewGroup) {
+            for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
+                View innerView = ((ViewGroup) view).getChildAt(i);
+                setupUI(innerView);
+            }
+        }
+    }
+
+    /**
+     * Reference: Navneeth G - https://stackoverflow.com/questions/4165414/how-to-hide-soft-keyboard-on-android-after-clicking-outside-edittext
+     * @param activity -  The current activity
+     */
+    public static void hideSoftKeyboard(Activity activity) {
+        InputMethodManager inputMethodManager =
+                (InputMethodManager) activity.getSystemService(
+                        Activity.INPUT_METHOD_SERVICE);
+        if (inputMethodManager == null) return;
+        View currentFocus = activity.getCurrentFocus();
+        if (currentFocus == null) return;
+        inputMethodManager.hideSoftInputFromWindow(
+                currentFocus.getWindowToken(), 0);
+    }
+
     private void initInputBox() {
-        EditText inputBox = findViewById(R.id.inputTextBox);
         inputBox.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
@@ -89,7 +131,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void outputButtonClick(View view) {
-        hideSoftKeyboard(this);
         Button outputButton = findViewById(R.id.buttonOutput);
         String outputString = outputButton.getText().toString();
         int speechStatus = textToSpeech.speak(outputString, TextToSpeech.QUEUE_FLUSH, null);
@@ -99,43 +140,34 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public void setupUI(View view) {
-
-        // Set up touch listener for non-text box views to hide keyboard.
-        if (!(view instanceof EditText)) {
-            view.setOnTouchListener(new View.OnTouchListener() {
-                public boolean onTouch(View v, MotionEvent event) {
-                    hideSoftKeyboard(MyActivity);
-                    return false;
-                }
-            });
+    /**
+     * Reference: https://www.tutorialspoint.com/how-to-integrate-android-speech-to-text
+     * @param view - Necessary argument for button click methods.
+     */
+    public void speakButtonClick(View view) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        try {
+            startActivityForResult(intent, TTS_REQUEST);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    "Sorry your device is not supported",
+                    Toast.LENGTH_SHORT).show();
         }
+    }
 
-        //If a layout container, iterate over children and seed recursion.
-        if (view instanceof ViewGroup) {
-            for (int i = 0; i < ((ViewGroup) view).getChildCount(); i++) {
-                View innerView = ((ViewGroup) view).getChildAt(i);
-                setupUI(innerView);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TTS_REQUEST) {
+            if (resultCode == RESULT_OK && null != data) {
+                ArrayList result = data
+                        .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                if (result == null) return;
+                inputBox.setText(result.get(0).toString());
             }
         }
     }
 
-    /**
-     * Reference: Navneeth G - https://stackoverflow.com/questions/4165414/how-to-hide-soft-keyboard-on-android-after-clicking-outside-edittext
-     * @param activity -  The current activity
-     */
-    public static void hideSoftKeyboard(Activity activity) {
-        InputMethodManager inputMethodManager =
-                (InputMethodManager) activity.getSystemService(
-                        Activity.INPUT_METHOD_SERVICE);
-        if (inputMethodManager == null) {
-            return;
-        }
-        View currentFocus = activity.getCurrentFocus();
-        if (currentFocus == null) {
-            return;
-        }
-        inputMethodManager.hideSoftInputFromWindow(
-                currentFocus.getWindowToken(), 0);
-    }
 }
