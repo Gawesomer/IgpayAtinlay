eec3bb1f5a0c6990b30378a89f7d87de570067be
Gawesomer <jj.jouband@gmail.com>
2019-11-21 12:24:37 -0600

    Replaced output text view with button and implemented text to speech.

--- app/src/main/java/com/example/igpayatinlay/MainActivity.java
+++ app/src/main/java/com/example/igpayatinlay/MainActivity.java
@@ -3,20 +3,53 @@ package com.example.igpayatinlay;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextToSpeech textToSpeech;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        initTextToSpeech();
         initInputBox();
+    }
+
+    /**
+     * Reference: https://javapapers.com/android/android-text-to-speech-tutorial/
+     */
+    private void initTextToSpeech() {
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS) {
+                    int ttsLang = textToSpeech.setLanguage(Locale.US);
+
+                    if (ttsLang == TextToSpeech.LANG_MISSING_DATA
+                            || ttsLang == TextToSpeech.LANG_NOT_SUPPORTED) {
+                        Log.e("TTS", "The Language is not supported!");
+                    } else {
+                        Log.i("TTS", "Language Supported.");
+                    }
+                    Log.i("TTS", "Initialization success.");
+                } else {
+                    Toast.makeText(getApplicationContext(), "TTS Initialization failed!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     private void initInputBox() {
@@ -34,13 +67,24 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
-                setOutputBox(s.toString());
+                setOutputButton(s.toString());
             }
         });
     }
 
-    private void setOutputBox(String input) {
-        TextView outputBox = findViewById(R.id.outputTextBox);
-        outputBox.setText(input);
+
+    private void setOutputButton(String input) {
+        Button outputButton = findViewById(R.id.buttonOutput);
+        outputButton.setText(input);
+    }
+
+    public void outputButtonClick(View view) {
+        Button outputButton = findViewById(R.id.buttonOutput);
+        String outputString = outputButton.getText().toString();
+        int speechStatus = textToSpeech.speak(outputString, TextToSpeech.QUEUE_FLUSH, null);
+
+        if (speechStatus == TextToSpeech.ERROR) {
+            Log.e("TTS", "Error in converting Text to Speech!");
+        }
     }
 }
