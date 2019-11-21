5e1b89fdb654421e12ea0f371088267a4de56dbd
Gawesomer <jj.jouband@gmail.com>
2019-11-21 11:53:39 -0600

    Added EditText and TextView. Text input into EditText gets copied into TextView.

--- app/src/main/java/com/example/igpayatinlay/MainActivity.java
+++ app/src/main/java/com/example/igpayatinlay/MainActivity.java
@@ -3,6 +3,11 @@ package com.example.igpayatinlay;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initInputBox();
+    }
+
+    private void initInputBox() {
+        EditText inputBox = findViewById(R.id.inputTextBox);
+        inputBox.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                setOutputBox(s.toString());
+            }
+        });
+    }
+
+    private void setOutputBox(String input) {
+        TextView outputBox = findViewById(R.id.outputTextBox);
+        outputBox.setText(input);
     }
 }
